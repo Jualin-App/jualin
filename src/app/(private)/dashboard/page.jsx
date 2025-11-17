7f0e6bf29becb70ab75eb02c13eade7edf8b6922
aryojpamungkas <aryopamungkas2004@gmail.com>
2025-11-17 23:47:45 +0700

    feat:navbar and footer

--- src/app/(private)/dashboard/page.jsx
+++ src/app/(private)/dashboard/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useContext } from "react";
 import { AuthContext } from "../../../context/AuthProvider.jsx";
-import Header from "./sections/header.jsx";
 import SearchBarSection from "./sections/searchbar.jsx";
 import BannerSection from "./sections/banner.jsx";
 import CategorySection from "./sections/category.jsx";
@@ -13,7 +12,6 @@ function DashboardPage() {
 
   return (
     <div className="min-h-screen bg-[#fafafa]">
-      <Header user={user} />
       <SearchBarSection />
       <main className="max-w-6xl mx-auto px-2 sm:px-4 py-4 sm:py-8">
         <BannerSection banners={banners}/>
