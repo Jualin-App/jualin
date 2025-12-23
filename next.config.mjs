97ee3361ce0eae51074226cdf5c3786f95d6a1ca
rkhplace <mrakhaptatama135@gmail.com>
2025-12-24 02:21:40 +0700

    chore : deployment fic nect.config.mjs v3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracing: false,
-  experimental: {
-  }
+  // JANGAN gunakan output: 'standalone' di Vercel
+  // JANGAN gunakan outputFileTracing: false (karena bug sudah fix di versi baru)
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        // Pastikan ini mengarah ke URL backend production Anda
+        destination: 'https://api.jualin-tel.biz.id/api/v1/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
